83fa1d67e78bcff3661c5a5d2399cfcceb66e57c
Pixel <73426556+PixelGames3786@users.noreply.github.com>
2022-02-21 03:18:51 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@
       var posi = jQuery(this).offset().top;
       var scroll = jQuery(window).scrollTop();
       if (scroll + winheight > posi){
-        jQuery(this).addClass("scrollin");
+        jQuery(this).addClass('scrollin');
       } else {
         //　スクロールで画面上部に戻った際に要素を非表示にしたい場合は、下記の行のコメントを外し有効にしてください。
         //jQuery(this).removeClass('scrollin');
