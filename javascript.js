2841d339ea54f02a5cc0b43794270deb93c42bb8
Pixel <73426556+PixelGames3786@users.noreply.github.com>
2022-02-20 21:22:48 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,15 @@
 $(function(){
   
-    setTimeout(
-    function() 
-    {
-        $('.beginscrollin').addClass('scrollin');
-    }, 100);
-  
   $(window).scroll(function (){
     $('.fadein').each(function(){
       var elementTop = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > elementTop - windowHeight + 100){
+      if (scroll > elementTop - windowHeight + 100)
+      {
         $(this).addClass('scrollin');
-      };
+      }
     });
   });
+  
 });
