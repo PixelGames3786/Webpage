1aa46a01601099e90ef48adb4ca116aaaa9f3fd8
Pixel <73426556+PixelGames3786@users.noreply.github.com>
2022-02-21 01:00:16 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
-$(function(){
-  $(window).scroll(function (){
-    $('.fadein').each(function(){
-      var elementTop = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > elementTop - windowHeight + 10){
-        $(this).addClass('scrollin');
+ <script>
+(window.onload = function() {
+
+  // フェードイン処理
+  jQuery(window).scroll(function (){
+    jQuery(".fadein").each(function(){
+      var winheight = jQuery(window).height();
+      var posi = jQuery(this).offset().top;
+      var scroll = jQuery(window).scrollTop();
+      if (scroll + winheight > posi){
+        jQuery(this).addClass("scrollin");
+      } else {
+        //　スクロールで画面上部に戻った際に要素を非表示にしたい場合は、下記の行のコメントを外し有効にしてください。
+        //jQuery(this).removeClass("fadein");
       }
     });
-  });
-});
+   });
+
+})(); 
+</script>
