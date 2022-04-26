66e900607977defeaba4629bfc1beb7858788867
Pixel <73426556+PixelGames3786@users.noreply.github.com>
2022-04-26 23:13:55 +0900

    1.0

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
 
-  // フェードイン処理
-  $(window).scroll(function (){
-    $('.fadein').each(function(){
-      var elementTop = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > elementTop - windowHeight + 100){
-        $(this).addClass('scrollin');
-      }
+$(document).ready(function () {
+
+    // フェードイン処理
+    $(window).scroll(function () {
+        $('.fadein').each(function () {
+            var elementTop = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elementTop - windowHeight + 100) {
+                $(this).addClass('scrollin');
+            }
+        });
     });
-   });
-
-}); 
+});
